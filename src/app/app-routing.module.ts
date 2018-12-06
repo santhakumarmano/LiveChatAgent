677295f1fbf859adde8ba9e393f011f2737f346b
infocuittesting <infocuit.testing@gmail.com>
2018-12-06 17:03:10 +0530

    all changes agent app fully created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+},
+{
+  path: 'login',
+  loadChildren: './login/login.module#LoginModule'
+},
+{
+  path:'Chat',
+  loadChildren:'./chat/chat.module#ChatModule'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/chat/chat/chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import {ChatService} from './chat.service';
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+  constructor( private ChatService:ChatService) { }
+
+  ngOnInit() {
+  }
+
+}
